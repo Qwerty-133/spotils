99b3c5499790eb6067556b5921d88f91dadea563
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2022-11-21 17:34:09 +0000

    transition to click

--- spotils/cli.py
+++ spotils/cli.py
@@ -1,30 +1,34 @@
 """Defines the CLI interface."""
-import typer
+import click
 
 from spotils.helpers.scheduler import run_tasks
 from spotils.utils.recently_played import print_recently_played_tracks
 
-app = typer.Typer(
-    no_args_is_help=True, help="A collection of different spotify utilities."
-)
 
-recent_limit_option = typer.Option(
-    50, "--limit", "-l", help="Max number of results", min=1, max=50
+@click.group(
+    no_args_is_help=True,
+    epilog="Run 'spotils command --help' to get help on a particular command.",
+    context_settings=dict(help_option_names=["-h", "--help"]),
 )
+@click.version_option(None, "-v", "--version", package_name=__package__)
+def app() -> None:
+    """A collection of different spotify utilities."""
 
 
 @app.command()
-def recent(limit: int = recent_limit_option) -> None:
+@click.option(
+    "-l",
+    "--limit",
+    help="Max number of results.",
+    default=50,
+    type=click.IntRange(min=1, max=50),
+)
+def recent(limit: int) -> None:
     """Display recently streamed tracks."""
     print_recently_played_tracks(limit)
 
 
 @app.command()
 def run() -> None:
-    """Run all scheduled tasks."""
+    """Run all the enabled tasks."""
     run_tasks()
-
-
-playlist_option = typer.Option(
-    None, "--playlist", "-p", help="The target playlist."
-)
